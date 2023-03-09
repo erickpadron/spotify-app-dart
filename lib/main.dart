e21a69c6ba43d2e0d22399dfec42fbdc117fc7ce
Érick Enrique Ramírez Padrón <A325850@alumnos.uaslp.mx>
2023-03-09 10:44:16 -0600

    Avance 1, agregué los botones de reproducidos recientemente...

--- lib/main.dart
+++ lib/main.dart
@@ -5,48 +5,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Good evening'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +30,328 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  double radiusRecent = 10.0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+        backgroundColor: Colors.black87,
+        title: const Row(
+          children: [
+            SizedBox(width: 10.0),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Good evening',
+              style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            Spacer(),
+            Icon(Icons.notifications_none, color: Colors.white),
+            SizedBox(width: 15.0),
+            Icon(Icons.access_time, color: Colors.white),
+            SizedBox(width: 15.0),
+            Icon(Icons.settings, color: Colors.white),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                width: 80.0,
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(42, 42, 42, 1),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Music',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(5.0),
+                width: 160.0,
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(42, 42, 42, 1),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Podcasts & Shows',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // Cuadro de recientes
+          Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0, top: 10.0),
+                    width: MediaQuery.of(context).size.width/2-20,
+                    height: 65.0,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(42, 42, 42, 1),
+                      borderRadius: BorderRadius.circular(radiusRecent),
+                    ),
+                    child: Center(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 10.0),
+                              width: 65.0,
+                              height: 65.0,
+                              decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(radiusRecent),
+                                  bottomLeft: Radius.circular(radiusRecent),
+                                ),
+                              ),
+                            ),
+                            const Expanded(
+                              child: Text(
+                                'Liked Songs',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0, top: 10.0),
+                    width: MediaQuery.of(context).size.width/2-20,
+                    height: 65.0,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(42, 42, 42, 1),
+                      borderRadius: BorderRadius.circular(radiusRecent),
+                    ),
+                    child: Center(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 10.0),
+                              width: 65.0,
+                              height: 65.0,
+                              decoration: BoxDecoration(
+                                color: Colors.pink,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(radiusRecent),
+                                  bottomLeft: Radius.circular(radiusRecent),
+                                ),
+                              ),
+                            ),
+                            const Expanded(
+                              child: Text(
+                                'Musculok 2',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0, top: 7.0),                    width: MediaQuery.of(context).size.width/2-20,
+                    height: 65.0,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(42, 42, 42, 1),
+                      borderRadius: BorderRadius.circular(radiusRecent),
+                    ),
+                    child: Center(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 10.0),
+                              width: 65.0,
+                              height: 65.0,
+                              decoration: BoxDecoration(
+                                color: Colors.brown,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(radiusRecent),
+                                  bottomLeft: Radius.circular(radiusRecent),
+                                ),
+                              ),
+                            ),
+                            const Expanded(
+                              child: Text(
+                                'This is Kim Petras',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0, top: 7.0),
+                    width: MediaQuery.of(context).size.width/2-20,
+                    height: 65.0,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(42, 42, 42, 1),
+                      borderRadius: BorderRadius.circular(radiusRecent),
+                    ),
+                    child: Center(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 10.0),
+                              width: 65.0,
+                              height: 65.0,
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(radiusRecent),
+                                  bottomLeft: Radius.circular(radiusRecent),
+                                ),
+                              ),
+                            ),
+                            const Expanded(
+                              child: Text(
+                                '2010s Mix',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0, top: 7.0),
+                    width: MediaQuery.of(context).size.width/2-20,
+                    height: 65.0,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(42, 42, 42, 1),
+                      borderRadius: BorderRadius.circular(radiusRecent),
+                    ),
+                    child: Center(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 10.0),
+                              width: 65.0,
+                              height: 65.0,
+                              decoration: BoxDecoration(
+                                color: Colors.purple,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(radiusRecent),
+                                  bottomLeft: Radius.circular(radiusRecent),
+                                ),
+                              ),
+                            ),
+                            const Expanded(
+                              child: Text(
+                                'On Repeat',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0, top: 7.0),
+                    width: MediaQuery.of(context).size.width/2-20,
+                    height: 65.0,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(42, 42, 42, 1),
+                      borderRadius: BorderRadius.circular(radiusRecent),
+                    ),
+                    child: Center(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 10.0),
+                              width: 65.0,
+                              height: 65.0,
+                              decoration: BoxDecoration(
+                                color: Colors.yellow,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(radiusRecent),
+                                  bottomLeft: Radius.circular(radiusRecent),
+                                ),
+                              ),
+                            ),
+                            const Expanded(
+                              child: Text(
+                                'Mr. Morale & The Big Steppers',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            color: Colors.red[600],
+            width: MediaQuery.of(context).size.width,
+            height: 250.0,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            color: Colors.amber[600],
+            width: 48.0,
+            height: 48.0,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            color: Colors.amber[600],
+            width: 48.0,
+            height: 48.0,
+          ),
+        ],
+      ),
+      backgroundColor: Colors.black87,
     );
   }
 }
